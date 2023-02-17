f82076706804ca43e777ba74d67e0afb2062127b
Ali-shakib23 <alishakib2310@student.aast.edu>
2023-02-16 17:44:50 -0800

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 /*
- *  main - Entry point
- * Description : program that prints using printf 
- * Return : 0 to terminate the program
+ * main - Entry point
+ *Description : program that prints using printf 
+ *Return : 0 to terminate the program
  */
 
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,");
-	return(0);
+	return (0);
 }
