54bb331bf707efa3c752b2b495003669f07903c4
Ali-shakib23 <alishakib2310@student.aast.edu>
2023-02-17 15:57:30 -0800

    lowercase and uppercase

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: lowercase and uppercase
+ *
+ * Return: 0 to terminate the program
+ */
+int main(void)
+{
+	int ch1 = 97;
+	int ch2 = 65;
+
+	while (ch1 <= 'z')
+	{
+		putchar(ch1);
+	}
+	while (ch2 < 'Z')
+	{
+		putchar(ch2);
+	}
+	putchar('\n');
+	return (0);
+}
