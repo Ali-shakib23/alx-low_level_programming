2841cd640d7fd7603bf4fbd82a858bf421772869
Ali-shakib23 <alishakib2310@student.aast.edu>
2023-02-16 17:35:21 -0800

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/*
+ *  main - Entry point
+ * Description : program that prints using printf 
+ * Return : 0 to terminate the program
+ */
+
+int main (void)
+{
+	prinft("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
