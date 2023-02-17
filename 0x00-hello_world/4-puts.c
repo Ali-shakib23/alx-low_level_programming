24ab72adb0fa4169727dbc1aa39ba892b8d044f8
Ali-shakib23 <alishakib2310@student.aast.edu>
2023-02-16 16:50:41 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,8 @@
 #include<stdio.h>
+/*
+ *main- function where the program runs first
+ *return 0: terminate the program 
+ */
 int main(void) 
 {
 	puts("\"Programming is like building a multilingual puzzle");
