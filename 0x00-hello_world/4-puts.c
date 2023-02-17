b6d3bd21981086ac7b6cb4588d74f73c850f18a7
Ali-shakib23 <alishakib2310@student.aast.edu>
2023-02-16 17:04:33 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,11 @@
 #include<stdio.h>
+
 /**
  * main - function where the program runs first
+ * Description: a programm that prints "\"programming is like building a multilignual puzzle"
  * return :0 to terminate the program 
  */
+
 int main(void) 
 {
 	puts("\"Programming is like building a multilingual puzzle");
