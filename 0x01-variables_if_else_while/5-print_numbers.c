388d1545cbbdbb17e21341f0ecf0cedf00048e70
Ali-shakib23 <alishakib2310@student.aast.edu>
2023-02-17 17:51:52 -0800

    number of base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: single digits of base 10
+ *
+ * Return: 0 to terminate the program
+ */
+int main(void)
+{
+	int num;
+	num = 0;
+
+	while (num < 10)
+	{
+		printf("%d", num);
+		num++
+	}
+	printf('\n');
+	return (0);
+}
