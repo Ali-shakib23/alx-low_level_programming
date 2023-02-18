8aaf609420d3c030b248f2bd7c2d707c35d60216
Ali-shakib23 <alishakib2310@student.aast.edu>
2023-02-17 18:22:09 -0800

    numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: lowercase and uppercase
+ *
+ * Return: 0 to terminate the program
+ */
+int main(void)
+{
+	int chr;
+
+	chr = 0;
+	while(chr < 10)
+	{
+		putchar(a + '0');
+	}
+	putchar('\n');
+	return (0);
+}
