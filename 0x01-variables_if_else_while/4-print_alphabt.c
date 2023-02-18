7426bac90818da4361712bfd92eb5a095fc76f82
Ali-shakib23 <alishakib2310@student.aast.edu>
2023-02-17 16:11:30 -0800

    lowercase with condition

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: lowercase and uppercase except q and e
+ *
+ * Return: 0 to terminate the program
+ */
+int main(void)
+{
+        int ch = 97;
+
+        while (ch <= 'z')
+        {
+		if (ch == 'e' || ch == 'q')
+		{
+			continue;
+		}
+                putchar(ch1);
+                ch++;
+        }
+        putchar('\n');
+        return (0);
+}
