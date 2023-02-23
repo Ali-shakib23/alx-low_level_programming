594e4cf9d5ce1e1ff4bfcb0b060490de08bcc9b2
Ali-shakib23 <alishakib2310@student.aast.edu>
2023-02-22 16:59:34 -0800

    check if lowercase

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,11 +2,13 @@
 /**
  * _islower - check if lowercase
  *
+ * c - asci code character
+ *
  * Return: 0 to terminate the program
  */
 int _islower(int c)
 {
-	if (c >= 'a' || c <= 'z')
+	if (c >= 'a' && c <= 'z')
 	{
 		return (1);
 	}
