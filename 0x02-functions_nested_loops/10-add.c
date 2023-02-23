f1c8c4206962712ef50079845d56cdd5bdccb8e1
Ali-shakib23 <alishakib2310@student.aast.edu>
2023-02-22 18:45:17 -0800

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * add - add two numbers 
+ *
+ * @n1, @n2: two input number as integer
+ *
+ * Return: 0 to terminate the program
+ */
+int add(int n1, int n2)
+{
+	int sum;
+
+	sum = n1+ n2;
+	return (sum);
+}
