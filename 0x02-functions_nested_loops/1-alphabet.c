ec16823625eddef52af3f5940077a8b42a7dee4f
Ali-shakib23 <alishakib2310@student.aast.edu>
2023-02-22 16:06:58 -0800

    lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Description: print in lower case
+ *
+ * Return: 0 to terminate the program
+ */
+void print_alphabet(void)
+{
+	int i;
+	for (i=97;i<=122;i++)
+	{
+		_putcahr(i);
+	}
+	_putchar('\n');
+}
