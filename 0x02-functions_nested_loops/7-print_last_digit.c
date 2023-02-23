4267799cba0981420ea6143e015e2f6193e06129
Ali-shakib23 <alishakib2310@student.aast.edu>
2023-02-22 18:35:30 -0800

    last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -11,7 +11,15 @@ int print_last_digit(int n)
 {
 	int m;
 
-	m= n % 10;
-	printf ("%d", m);
-	return (m);
+	m = n % 10;
+	if (m < 0)
+	{
+		_putchar(-m + 48);
+		return (-m);
+	}
+	else
+	{
+		_putchar(m + 48);
+		return (m);
+	}
 }
