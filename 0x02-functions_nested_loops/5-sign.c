9c0d06669f2733602c061b5bfca960771fecaeb7
Ali-shakib23 <alishakib2310@student.aast.edu>
2023-02-22 17:54:22 -0800

    sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_sign : check if +ve , -ve or zero
+ *
+ * Return: 0 to terminate the program
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else
+	{
+		_putchar('0');
+	}
+	return (0);
+}
