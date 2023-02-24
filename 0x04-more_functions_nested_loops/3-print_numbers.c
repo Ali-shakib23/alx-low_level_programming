fcf9f1d8b8ae76b8a3b9176c286ce17d77777a04
Ali-shakib23 <alishakib2310@student.aast.edu>
2023-02-24 07:31:03 -0800

    from 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_numbers- print from 0 to 89
+ *
+ * Return: no return since it is void
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i=0;i <= 9; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+}
