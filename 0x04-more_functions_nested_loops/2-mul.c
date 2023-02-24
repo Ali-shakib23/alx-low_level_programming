0c8c9095953bb7eb9919cda845cce0b4a90d8f34
Ali-shakib23 <alishakib2310@student.aast.edu>
2023-02-24 07:24:00 -0800

    multipication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * mul - Entry point
+ *
+ * @a: first number as  paramter
+ *
+ * @b: second number as parameter
+ *
+ * Return: result of mulipication
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
